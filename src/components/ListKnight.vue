b68d7e764ee3577848c422aad604305747b1f3d8
Rafael Garcia <rushxpush@gmail.com>
2025-01-24 21:29:44 -0300

    feat: add button to toggle kight's and hero's list

--- src/components/ListKnight.vue
+++ src/components/ListKnight.vue
@@ -2,6 +2,12 @@
   <div class="container">
     <h1>Lista de Knights</h1>
     <div class="container-knights-list">
+      <div class="container-knights-hero-toggles">
+        <button @click="handleToggleKnightHeroList()">
+          <p v-if="showKnightsList">Mostrar Heróis</p>
+          <p v-else>Mostrar Knights</p>
+        </button>
+      </div>
       <table>
         <thead>
           <tr>
@@ -9,8 +15,8 @@
             <th>Idade</th>
             <th>Armas</th>
             <th>Atributo</th>
-            <th>Ataque</th>
-            <th>Exp</th>
+            <th v-if="showKnightsList">Ataque</th>
+            <th v-if="showKnightsList">Exp</th>
           </tr>
         </thead>
         <tbody>
@@ -19,9 +25,9 @@
             <td>{{ knight.age }}</td>
             <td>{{ knight.weapons.length }}</td>
             <td>{{ knight.keyAttribute }}</td>
-            <td>{{ knight.attack }}</td>
-            <td>{{ knight.experience }}</td>
-            <td><button @click="handleRemoveKnight(knight._id)" class="remove-button">Remover Knight</button></td>
+            <td v-if="showKnightsList">{{ knight.attack }}</td>
+            <td v-if="showKnightsList">{{ knight.experience }}</td>
+            <td v-if="showKnightsList"><button @click="handleRemoveKnight(knight._id)" class="remove-button">Remover Knight</button></td>
           </tr>
         </tbody>
       </table>
@@ -34,7 +40,8 @@
 import type { FetchedKnight } from '@/interfaces/fetched-knight.interface';
 import { onMounted, ref, type Ref } from 'vue';
 
-  const url = 'http://localhost:3000/knights/'
+  const url = 'http://localhost:3000/knights'
+  const showKnightsList = ref(true);
 
   const knights: Ref<FetchedKnight[]> = ref([]);
   const handleFetchKnightsList = async () => {
@@ -45,6 +52,17 @@ import { onMounted, ref, type Ref } from 'vue';
     } catch(error) {
       console.log('Error: ', error)
     }
+  }
+
+  const handleFetchHeroesList = async () => {
+    try {
+      const response = await fetch(url + '?filter=heroes');
+      const data: FetchedKnight[] = await response.json();
+      knights.value = data;
+    } catch(error) {
+      console.log('Error: ', error)
+    }
+
   }
 
   const handleRemoveKnight = async (_id: string) => {
@@ -60,15 +78,35 @@ import { onMounted, ref, type Ref } from 'vue';
     }
   }
 
+  const handleToggleKnightHeroList = async () => {
+    showKnightsList.value = !showKnightsList.value;
+    if (showKnightsList.value) {
+      handleFetchKnightsList()
+    }
+    else {
+      handleFetchHeroesList()
+    }
+
+  }
+
   onMounted(() => {
     handleFetchKnightsList();
   })
 </script>
 
 <style scoped>
+
+.container-knights-hero-toggles {
+  display: flex;
+  gap: 10px;
+  margin-bottom: 10px;
+
+
+}
 .remove-button {
   background-color: rgb(129, 17, 17);
   color: rgb(255, 255, 255);
+  min-width: 150px;
 }
 .remove-button:hover {
   background-color: rgb(232, 65, 65);
